53c5ab6c8e42f0d8838a5808c4ad0888cc935b2c
huangkerui <huangkerui@gmail.com>
2016-09-22 22:14:31 -0400

    add more comments in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ var webpack = require('webpack');
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
+  entry: "./js/scripts.js", // where we start off, the root of the depdency tree
   output: {
     path: __dirname + "/js",
-    filename: "scripts.min.js"
+    filename: "scripts.min.js" // the output js file -- the new entry point
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  ], // if in prod mode, minify the scripts.
 };
