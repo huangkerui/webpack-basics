38caa90842075bbebeed045da4ae2f672ba048ce
huangkerui <huangkerui@gmail.com>
2016-09-22 23:05:02 -0400

    fix the wrong entry in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ var webpack = require('webpack');
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js", // where we start off, the root of the depdency tree
+  entry: "./js/script.js", // where we start off, the root of the depdency tree
   output: {
     path: __dirname + "/js",
-    filename: "scripts.min.js" // the output js file -- the new entry point
+    filename: "script.min.js" // the output js file -- the new entry point
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
